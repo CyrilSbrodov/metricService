2902e92d94799959aa5e53dff22ea357fc23c1d8
CyrilSbrodov <sbrandy@bk.ru>
2022-09-24 13:22:39 +0300

    the first increment is competed

---
+++ internal/handlers/handler.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type User struct {
+	ID        string
+	FirstName string
+	LastName  string
+}
+
+func UserViewHandler(users map[string]User) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		userId := r.URL.Query().Get("user_id")
+		if r.URL.Query().Get("user_id") == "" {
+			http.Error(rw, "userId is empty", http.StatusBadRequest)
+			return
+		}
+
+		user, ok := users[userId]
+		if !ok {
+			http.Error(rw, "user not found", http.StatusNotFound)
+			return
+		}
+
+		jsonUser, err := json.Marshal(user)
+		if err != nil {
+			http.Error(rw, "can't provide a json. internal error", http.StatusInternalServerError)
+			return
+		}
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		rw.Write(jsonUser)
+	}
+}
