c061e94a73d9f5e40685c984cfcdd93714c0223f
CyrilSbrodov <sbrandy@bk.ru>
2022-09-24 15:35:34 +0300

    the first increment is completed

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,20 +1,26 @@
 package main
 
+import (
+	"github.com/CyrilSbrodov/metricService.git/internal/handlers"
+	"log"
+	"net/http"
+)
+
 func main() {
 
-	//users := map[string]handlers.User{
-	//	"1": {
-	//		ID:        "1",
-	//		FirstName: "Test",
-	//		LastName:  "Test",
-	//	},
-	//	"2": {
-	//		ID:        "2",
-	//		FirstName: "Test 2",
-	//		LastName:  "Test 2",
-	//	},
-	//}
-	//http.HandleFunc("/user", handlers.UserViewHandler(users))
-	//
-	//log.Fatal(http.ListenAndServe(":8080", nil))
+	users := map[string]handlers.User{
+		"1": {
+			ID:        "1",
+			FirstName: "Test",
+			LastName:  "Test",
+		},
+		"2": {
+			ID:        "2",
+			FirstName: "Test 2",
+			LastName:  "Test 2",
+		},
+	}
+	http.HandleFunc("/user", handlers.UserViewHandler(users))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
