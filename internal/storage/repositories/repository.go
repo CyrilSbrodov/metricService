36e56f384d0cfb3481c7b09ae735aadeac14e0e1
CyrilSbrodov <sbrandy@bk.ru>
2022-10-14 12:07:20 +0300

    fix fourth increment

--- internal/storage/repositories/repository.go
+++ internal/storage/repositories/repository.go
@@ -8,13 +8,18 @@ import (
 
 type Repository struct {
 	Metrics map[string]storage.Metrics
-	gauge
+	Gauge   map[string]float64
+	Counter map[string]int64
 }
 
 func NewRepository() *Repository {
 	metrics := storage.MetricsStore
+	gauge := storage.GaugeData
+	counter := storage.CounterData
 	return &Repository{
 		Metrics: metrics,
+		Gauge:   gauge,
+		Counter: counter,
 	}
 }
 
@@ -64,3 +69,49 @@ func (r *Repository) GetAll() string {
 	}
 	return result
 }
+
+func (r *Repository) CollectOrChangeGauge(name string, value float64) error {
+
+	r.Gauge[name] = value
+
+	return nil
+}
+
+func (r *Repository) CollectOrIncreaseCounter(name string, value int64) error {
+	val, ok := r.Counter[name]
+	if !ok {
+		r.Counter[name] = value
+		return nil
+	}
+	r.Counter[name] = value + val
+
+	return nil
+}
+
+func (r *Repository) GetGauge(name string) (float64, error) {
+	value, ok := r.Gauge[name]
+	if !ok {
+		return value, fmt.Errorf("missing metric %s", name)
+	}
+	return value, nil
+}
+
+func (r *Repository) GetCounter(name string) (int64, error) {
+	value, ok := r.Counter[name]
+	if !ok {
+		return value, fmt.Errorf("missing metric %s", name)
+	}
+	return value, nil
+}
+
+//func (r *Repository) GetAll() string {
+//	result := "Gauge:\n"
+//	for s, f := range r.Gauge {
+//		result += fmt.Sprintf("%s : %f\n", s, f)
+//	}
+//	result = result + "Counter:\n"
+//	for s, i := range r.Counter {
+//		result += fmt.Sprintf("%s : %d\n", s, i)
+//	}
+//	return result
+//}
