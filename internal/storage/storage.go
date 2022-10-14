391e59218e5b552d127b1391670c4014384a2989
CyrilSbrodov <sbrandy@bk.ru>
2022-10-14 12:08:31 +0300

    fix fourth increment

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,4 +4,8 @@ type Storage interface {
 	GetMetric(metric *Metrics) (Metrics, error)
 	GetAll() string
 	CollectMetrics(store map[string]Metrics) error
+	CollectorChangeGauge(name string, value float64) error
+	CollectOrIncreaseCounter(name string, value int64) error
+	GetGauge(name string) (float64, error)
+	GetCounter(name string) (int64, error)
 }
