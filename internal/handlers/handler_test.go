35a94b786c58c856da99ffd519e7f69eedb71c83
CyrilSbrodov <sbrandy@bk.ru>
2022-11-01 20:44:33 +0300

    the 10th increment is completed

--- internal/handlers/handler_test.go
+++ internal/handlers/handler_test.go
@@ -354,6 +354,7 @@ func TestHandler_Ping(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			fmt.Println(flagDatabase)
 			request := httptest.NewRequest(http.MethodGet, tt.request, nil)
 			w := httptest.NewRecorder()
 			h := handlers.Handler{
