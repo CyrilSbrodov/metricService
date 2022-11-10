b6923886e2a1d9a84c094e81a01c7b716788f282
CyrilSbrodov <sbrandy@bk.ru>
2022-11-10 13:10:27 +0300

    add logger package

--- cmd/loggers/logger.go
+++ cmd/loggers/logger.go
@@ -25,6 +25,6 @@ func (l *Logger) LogInfo(key, value, msg string) {
 	l.logger.Info().Str(key, value).Msg(msg)
 }
 
-func (l *Logger) LogDebug(msg string) {
-
+func (l *Logger) LogDebug(key, value, msg string) {
+	l.logger.Debug().Str(key, value).Msg(msg)
 }
