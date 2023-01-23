a0fbef9ba8497b8ad8d0637a8f93c5847f9f4bf4
CyrilSbrodov <sbrandy@bk.ru>
2023-01-23 19:25:00 +0300

    increment 19 is completed

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/CyrilSbrodov/metricService.git/internal/app"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+	fmt.Printf("Build version: %s\nbuildDate: %s\nbuildCommit: %s\n", buildVersion, buildDate, buildCommit)
+
 	agent := app.NewAgentApp()
 	agent.Run()
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
 	_ "net/http/pprof"
 
 	"github.com/CyrilSbrodov/metricService.git/internal/app"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+	fmt.Printf("Build version: %s\nbuildDate: %s\nbuildCommit: %s\n", buildVersion, buildDate, buildCommit)
+
 	srv := app.NewServerApp()
 	srv.Run()
 }
